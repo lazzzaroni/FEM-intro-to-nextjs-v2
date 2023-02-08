c1707212cd1b6db9850a58590c1fe55ca6e5e841
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-02-08 12:48:12 +0100

    init commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import fs from "fs";
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>contact</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
